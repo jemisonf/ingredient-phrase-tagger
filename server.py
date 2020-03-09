5c5a3218a826454f4311c5144d56129634e6284f
Fischer Jemison <jemisonf@oregonstate.edu>
2020-03-08 20:17:56 -0700

    adds passable api code

---
+++ server.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+from ingredient_phrase_tagger.training import parse_ingredients
+
+from flask import Flask, request
+
+model_path = os.getenv("MODEL_PATH", "")
+
+app = Flask(__name__)
+
+@app.route("/ingredients", methods=["POST"])
+def ingredients():
+    if request.method == "POST":
+        body = request.get_json()
+        crf_output = parse_ingredients._exec_crf_test(body["data"], model_path)
+        results = json.loads(parse_ingredients._convert_crf_output_to_json(crf_output.rstrip().split("\n")))
+        print(results)
+        return {"data": results}
